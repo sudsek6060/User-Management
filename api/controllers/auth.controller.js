f0c684d0e8ad6c3192c5e44c32233831ea3d5989
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2024-03-01 15:51:10 +0530

    signin api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/errorHandler.js";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password, role } = req.body;
@@ -14,6 +16,28 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created sucessfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return next(errorHandler(404, "user not found"));
+    const userPassword = bcryptjs.compareSync(password, user.password);
+    if (!userPassword)
+      return next(errorHandler(401, "Please provide valid email or password"));
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = user._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
