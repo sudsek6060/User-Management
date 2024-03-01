55d80f0e3699d597cf34f375b796974471d75c92
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2024-03-01 17:46:49 +0530

    update user api created

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errorHandler.js";
 
 export const createUser = async (req, res, next) => {
   const { username, email, password, role, age, gender } = req.body;
@@ -14,6 +15,36 @@ export const createUser = async (req, res, next) => {
       gender,
     });
     const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) next(errorHandler(402, "user not found"));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          age: req.body.age,
+          gender: req.body.gender,
+          role: req.body.role,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
   } catch (error) {
     next(error);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 import { verifyUserRole } from "../utils/verifyUserRole.js";
 
 const router = express.Router();
 
 router.post("/create-user", verifyUser, verifyUserRole, createUser);
+router.post("/update-user/:id", verifyUser, verifyUserRole, updateUser);
 
 export default router;
