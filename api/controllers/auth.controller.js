ab564d68f2357a602d87ae24d1cc466356b10d84
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2024-03-01 19:03:22 +0530

    sign out api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,3 +42,12 @@ export const signIn = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("user sign out successfully");
+  } catch (error) {
+    next(error);
+  }
+};
