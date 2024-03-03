33ddc0ff3ba9393234784ff9276e6353acdae669
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2024-03-03 07:44:08 +0530

    about page created

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,69 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="max-w-[75%] mx-auto mt-4">
+      <h1 className="font-bold text-2xl">About</h1>
+      <p className="text-slate-500 mt-2">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+        animi sapiente voluptatibus, itaque, dolore, qui modi omnis repellat
+        eligendi vel exercitationem eos natus asperiores! Rerum, harum voluptas
+        voluptatibus aspernatur placeat saepe asperiores distinctio quo
+        voluptatem, deserunt perspiciatis, quisquam ea doloribus. Perferendis
+        eveniet numquam quasi debitis. Quibusdam voluptatem, magni nesciunt
+        quidem saepe incidunt ea amet reiciendis consequatur harum
+        necessitatibus cumque, tempora pariatur modi quis debitis fugiat autem
+        id at quae ipsa ex deleniti quisquam? Alias fugit delectus voluptas
+        commodi excepturi. Voluptas, recusandae magnam maxime ad reiciendis
+        corporis. Deleniti, in molestiae. Provident non accusamus quasi! Quia
+        hic veritatis consequuntur rem animi non id, odit maxime assumenda ipsa
+        aliquam, ea magnam voluptates? Quasi adipisci, ipsam mollitia
+        distinctio, eveniet veritatis maxime commodi nam corrupti, aut officia
+        blanditiis illum iure dignissimos. Maxime, porro labore. Dicta voluptas
+        ipsum accusamus molestias expedita eveniet itaque? Molestias nostrum
+        dolores, tempore quos delectus deserunt labore facere excepturi fuga
+        blanditiis nesciunt beatae reiciendis facilis aut deleniti hic eligendi
+        tempora sunt quis adipisci? Impedit molestiae odit similique, dolor
+        consequuntur labore dicta dolore fugiat quisquam fugit, alias harum quia
+        amet ipsam, ipsa architecto libero asperiores totam cupiditate. Cum
+        culpa maiores sapiente incidunt provident sint dolorum. Velit magni
+        aliquid delectus iste amet eligendi laboriosam, ratione in commodi
+        optio! Eaque eius veritatis ea adipisci dolores. Quaerat error tempore
+        sint sit tenetur, accusamus quis quasi cupiditate, vitae repellat modi
+        qui! Temporibus nisi itaque voluptates vel quia neque, dolorum velit
+        officiis quidem asperiores! Excepturi minus unde assumenda quaerat vel
+        officia impedit explicabo quo consequatur. Voluptates labore omnis
+        beatae suscipit! Impedit modi aspernatur error doloremque totam quaerat,
+        rem ducimus numquam, similique hic ea tempora, laudantium aliquam quos
+        quia ipsum deleniti. Eaque non corrupti unde! Ut aliquam quisquam
+        molestiae amet reiciendis soluta quaerat ullam esse delectus vel. Sunt,
+        dolorum voluptas eligendi eos dicta vero vitae iure quia quis
+        recusandae, esse ex sed suscipit! Voluptates ratione sunt impedit
+        adipisci perferendis? Deleniti delectus alias numquam inventore, tempore
+        quis consequuntur enim iure vitae eos ex repudiandae sit rerum similique
+        doloremque esse, architecto veritatis quam officia, excepturi voluptatem
+        placeat. Eius nostrum tenetur est atque excepturi ullam laborum culpa
+        odio numquam dolores asperiores, quis quae deleniti quibusdam,
+        laboriosam consectetur nam placeat iste? Illum nam nemo est labore quis
+        blanditiis enim vitae pariatur unde iusto voluptas nostrum odio,
+        laboriosam magnam soluta esse libero quasi quo, consequuntur repellendus
+        mollitia facere voluptatum. Mollitia possimus suscipit amet rerum, ab
+        reiciendis aliquam incidunt sint. Dolore doloremque eos optio minus
+        nesciunt fuga libero! Soluta praesentium hic odio non culpa aliquid quia
+        temporibus minima et nobis repellendus delectus excepturi, animi nostrum
+        debitis facilis rem illo quibusdam libero repellat? Consectetur maxime
+        magnam est fugit dolorum minus sit quaerat, modi nemo nulla! Blanditiis
+        hic dolorem dolore illum omnis cumque totam praesentium architecto
+        provident similique. Quisquam nesciunt quasi, dicta, minima magni enim
+        officia consequuntur voluptatum reprehenderit accusantium aliquid
+        debitis, similique error a tenetur illo! Perferendis non laborum est
+        eligendi recusandae! Assumenda atque minus dolorum fugit. Ipsum adipisci
+        magnam expedita, recusandae quas excepturi eveniet dolorem quos,
+        voluptatibus voluptas nulla, optio dignissimos autem tempore aspernatur.
+        Ipsum?
+      </p>
+    </div>
+  );
 };
 
 export default About;
